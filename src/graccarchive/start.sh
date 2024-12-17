b182bdcb433a72edd57ca2f0056bdf8ee3d70db4
Ashton Graves <agraves10@unl.edu>
2024-12-17 13:36:08 -0600

    Changes entrypoint

--- src/graccarchive/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python3 graccarchive.py -c "$1/gracc-archive-ps-itb.toml" & python3 graccarchive.py -c "$1/gracc-archive-ps-prod.toml" & python3 graccarchive.py -c "$1/gracc-archive-raw.toml" & python3 graccarchive.py -c "$1/gracc-archive-transfers.toml"
